85517d29ff74113b801303e919d3edb33474856f
muhammad.bilal <muhammad.bilal@safarifoneict.com>
2021-09-02 17:37:00 +0500

    added new features

--- src/serialNumbers.js
+++ src/serialNumbers.js
@@ -1,10 +1,6 @@
 
 const Nedb = require("./nedb")
 const db = new Nedb('./db/serialNumber.db').db
-//const fs = require('fs');
-
-// let serialNumberFile = './serialNumbers/serialNumber.txt'
-// let bookNumberFile = './serialNumbers/booknumber.txt'
 
 exports.getSerialNUmber = async function () {
     return new Promise((resolve, reject)=>{
@@ -16,7 +12,7 @@ exports.getSerialNUmber = async function () {
             if (docs && docs.length > 0) {
                 process.serialNumber = docs[0].serialNumber + 1
                 process.bookNumber = docs[0].bookNumber
-                
+
                 if (process.serialNumber > 100) {
                     process.serialNumber = 1
                     process.bookNumber++
@@ -46,26 +42,3 @@ exports.getSerialNUmber = async function () {
            });
      })
 }
-
-// exports.get = async function () {
-//     process.serialNumber =  +fs.readFileSync(serialNumberFile, {encoding:'utf8', flag:'r'})
-//     process.bookNumber = +fs.readFileSync(bookNumberFile, {encoding:'utf8', flag:'r'}) 
-// }
-
-// exports.setSerialNumber = async function () {
-//     process.serialNumber =  +fs.readFileSync(serialNumberFile, {encoding:'utf8', flag:'r'})
-//     process.bookNumber = +fs.readFileSync(bookNumberFile, {encoding:'utf8', flag:'r'}) 
-//     process.serialNumber++
-//     if (process.serialNumber > 100) {
-//         process.serialNumber = 1
-//         process.bookNumber++
-//         fs.writeFileSync(bookNumberFile, +process.bookNumber)
-//     }
-    
-//     fs.writeFileSync(serialNumberFile, +process.serialNumber)
-// }
-
-// exports.setBookNumber = async function () {
-    
-//     fs.writeFileSync(bookNumberFile, +process.bookNumber)
-// }
