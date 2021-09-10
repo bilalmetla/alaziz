1e715e8d864330f6ee4897526e1d6510db19be40
muhammad.bilal <muhammad.bilal@safarifoneict.com>
2021-09-10 21:41:18 +0500

    added missing db file folder

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express()
 //   }));
 app.use(cors())
 
-app.use( express.static( 'build'))
+app.use( express.static( 'public'))
 
 // app.get("/", (req, res) => {
 //   res.sendFile("index.html", {root:'build'});
